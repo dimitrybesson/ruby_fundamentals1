a3eef2eea1d550102f5007e26ce6c7dee58e11c4
Dimitry Besson <dimitrybesson@gmail.com>
2016-03-22 15:16:48 -0400

    changed comment about space string

--- exercise2.rb
+++ exercise2.rb
@@ -5,7 +5,7 @@ puts 55.0 * 1.2 # calculate total bill
 # 2. Adding string and integer
 #puts 5 + "hello" # TypeError, can't combine string with int
 puts 5.to_s + "hello"
-puts 5.to_s + " " + "hello" # with an empty string for space in between
+puts 5.to_s + " " + "hello" # with a space for space in between
 
 # 3. Outputting calculation in a string (string interpolation)
 puts "The output is: #{45628 * 7839}"
